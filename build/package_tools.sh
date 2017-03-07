db5e9327deb907abefde4d6a918c5a77128dc228
Dave Arnold <dave.arnold@chartboost.com>
2017-03-06 21:41:23 -0800

    blah

--- build/package_tools.sh
+++ build/package_tools.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export BUILD_VERSION=$(cat $TRAVIS_BUILD_DIR/package.json | jq -r .version)
+export BUILD_VERSION=$(cat $TRAVIS_BUILD_DIR/build/package.json | jq -r .version)
 export DEBIAN_PACKAGE="spinnaker-deployment-tools_${BUILD_VERSION}_amd64.deb"
 echo $DEBIAN_PACKAGE
 
