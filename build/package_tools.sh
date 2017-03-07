a33d2cd7e9b93024c113f63c8ed22e6372c85918
Dave Arnold <dave.arnold@chartboost.com>
2017-03-06 21:25:09 -0800

    changing execute permissions on package_tools.sh

--- build/package_tools.sh
+++ build/package_tools.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 
-brew install gnu-tar
-brew install jq
+apt-get install -y jq
 gem install deb-s3 fpm
 
-export BUILD_VERSION=$(cat package.json | jq -r .version)
-export DEBIAN_PACKAGE="exchange-admin_${BUILD_VERSION}_amd64.deb"
+export BUILD_VERSION=$(cat $TRAVIS_BUILD_DIR/package.json | jq -r .version)
+export DEBIAN_PACKAGE="spinnaker-deployment-tools_${BUILD_VERSION}_amd64.deb"
 echo $DEBIAN_PACKAGE
 
-fpm -s dir -t deb -n exchange-admin -v $BUILD_VERSION \
-	--before-install=./config/pre_install.sh \
-	--after-install=./config/post_install.sh \
-	--deb-pre-depends=nginx  \
-	build/=/opt/chartboost/applications/exchange-admin config/nginx/=/etc/nginx/sites-enabled
+fpm -s dir -t deb -n spinnaker-deployment-tools -v $BUILD_VERSION scripts/=/opt/spinnaker/tools/
 
-deb-s3 delete exchange-admin --versions=$BUILD_VERSION --arch amd64 --bucket=cb-devops-debs
+deb-s3 delete spinnaker-deployment-tools --versions=$BUILD_VERSION --arch amd64 --bucket=cb-devops-debs
 deb-s3 upload --bucket cb-devops-debs "$DEBIAN_PACKAGE" -e -v private
