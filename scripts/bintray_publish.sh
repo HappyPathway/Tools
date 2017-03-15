2a55c3190b64167fdb2d03d576aff392011eb39c
Dave Arnold <dave.arnold@chartboost.com>
2017-03-15 15:01:12 -0700

    craptastic

--- scripts/bintray_publish.sh
+++ scripts/bintray_publish.sh
@@ -10,7 +10,7 @@ echo $PACKAGE_CONFIG
 export BUILD_VERSION=$(cat $PACKAGE_CONFIG | jq -r .packageVersion)
 export BINTRAY_PACKAGE=$(cat $PACKAGE_CONFIG | jq -r .bintray_package)
 export BINTRAY_REPO=$(cat $PACKAGE_CONFIG | jq -r .bintray_repo)
-export DEBIAN_PACKAGE="$BINTRAY_PACKAGE_${BUILD_VERSION}_amd64.deb"
+export DEBIAN_PACKAGE="${BINTRAY_PACKAGE}_${BUILD_VERSION}_amd64.deb"
 export DEBIAN_ARCH=$(cat $PACKAGE_CONFIG | jq -r .arch)
 export DEBIAN_DISTRO=$(cat $PACKAGE_CONFIG | jq -r .distro)
 export DEBIAN_COMP=$(cat $PACKAGE_CONFIG | jq -r .component)
