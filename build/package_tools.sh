dcee7f3dbb7798b5b1f072f24ac0f45b128523c6
Dave Arnold <dave.arnold@chartboost.com>
2017-03-17 11:14:28 -0700

    can now create different branches of spinnaker-deployment-tools

--- build/package_tools.sh
+++ build/package_tools.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo "This is the latest!"
-
 export BUILD_VERSION=$(cat $TRAVIS_BUILD_DIR/build/package.json | jq -r .version)
 export BINTRAY_PACKAGE=$(cat $TRAVIS_BUILD_DIR/build/package.json | jq -r .bintray_package)
 export BINTRAY_REPO=$(cat $TRAVIS_BUILD_DIR/build/package.json | jq -r .bintray_repo)
@@ -11,9 +9,20 @@ export DEBIAN_DISTRO=$(cat $TRAVIS_BUILD_DIR/build/package.json | jq -r .distro)
 export DEBIAN_COMP=$(cat $TRAVIS_BUILD_DIR/build/package.json | jq -r .component)
 export PACKAGE_PATH=$TRAVIS_BUILD_DIR/$1;
 
-echo $DEBIAN_PACKAGE
+# echo $DEBIAN_PACKAGE
 
-fpm -s dir -t deb -n spinnaker-deployment-tools -v $BUILD_VERSION -a amd64 scripts/=/opt/spinnaker/tools/
+function pkg_name {
+	if [ "$TRAVIS_BRANCH" == "master" ]
+		then
+			echo "spinnaker-deployment-tools"
+		elif [[ -z $TRAVIS_BRANCH ]]; then
+			echo "spinnaker-deployment-tools"
+		else
+			echo "spinnaker-deployment-tools-${TRAVIS_BRANCH}"
+	fi
+}
+
+fpm -s dir -t deb -n $(pkg_name) -v $BUILD_VERSION -a amd64 scripts/=/opt/spinnaker/tools/
 
 if $(echo "${OSTYPE}" | grep -q darwin); then
     brew install awscli
